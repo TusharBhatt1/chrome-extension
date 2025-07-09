c42e03614e27556783a2bd97f9ec15d244412cd2
Tushar <tusharbhatt0135@gmail.com>
2025-07-09 16:32:19 +0530

    hiding content script for now

--- entrypoints/content.tsx
+++ entrypoints/content.tsx
@@ -4,18 +4,18 @@ import App from "@/entrypoints/popup/App";
 export default defineContentScript({
 	matches: ['https://www.hey.com/*'],
 	main(ctx) {
-		const ui = createIntegratedUi(ctx, {
-			position: "inline",
-			anchor: "body",
-			onMount: (container) => {
-				const root = ReactDOM.createRoot(container);
-				root.render(<App />);
-				return root;
-			},
-			onRemove: (root) => {
-				root?.unmount();
-			},
-		});
-		ui.mount();
+		// const ui = createIntegratedUi(ctx, {
+		// 	position: "inline",
+		// 	anchor: "body",
+		// 	onMount: (container) => {
+		// 		const root = ReactDOM.createRoot(container);
+		// 		root.render(<App />);
+		// 		return root;
+		// 	},
+		// 	onRemove: (root) => {
+		// 		root?.unmount();
+		// 	},
+		// });
+		// ui.mount();
 	},
 });
